5c28a3e25e75af1d89f9fd37e83d2e6c49aabd09
kaka <11076632@qq.com>
2025-05-17 16:19:07 +0800

    chore: 集成tailwind、shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { Button } from './components/ui/button';
 import A from '@/A';
 function App() {
-  return <A></A>;
+  return (
+    <>
+      <div>222</div>
+      <Button> 123 </Button>
+    </>
+  );
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import tailwindcss from '@tailwindcss/vite';
 
 // eslint把当前环境认成浏览器环境，我们需要重新定义一下__dirname
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
